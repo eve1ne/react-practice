23ea432cb472b5f347d033d60ec4c1c23967c16b
eve1ne <evelee@umich.edu>
2026-06-10 00:03:03 -0400

    worked on form

--- src/components/Inputer.jsx
+++ src/components/Inputer.jsx
@@ -1,23 +1,24 @@
 import '../index.css'
+import { useState } from "react"
 
 export default function Inputer() {
-    const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+    const [ingredients, setIngredients] = useState([])
 
     const ingredientsListItems = ingredients.map(ingredient => (
         <li key={ingredient}>{ingredient}</li>
     )) 
 
-    function handleSubmit(event) {
-        event.preventDefault()
-        const formData = new FormData(event.currentTarget)
+    function handleSubmit(formData) { // Use form action instead of onSubmit to avoid extra steps (clearing the submition)
         const newIngredient = formData.get("ingredient")
-        ingredients.push(newIngredient)
-        console.log(ingredients)
+        // ingredients.push(newIngredient) Can't edit the state (bad)
+
+        // Adds the new ingredient to the end of the list after the last previous ingredient that was added
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
     }
 
     return(
         <>
-            <form onSubmit={handleSubmit} className="add-ingredient-form">
+            <form action={handleSubmit} className="add-ingredient-form">
                 <input 
                     type="text"
                     placeholder="e.g. oregano"
